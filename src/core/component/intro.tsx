713736d0a3808ca8d273e09d4ca67863eeb076f9
Priyanshu Verma <priyanshuvr1308@gmail.com>
2025-12-20 23:41:04 +0530

    home-page_changes

--- src/core/component/intro.tsx
+++ src/core/component/intro.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Intro() {
   return (
-    <div className="relative h-120 w-full overflow-hidden my-5">
+    <div className="relative h-120 w-full overflow-hidden my-1 align-items-top">
       <Image
         src="/statue.png"
         alt="Library"
@@ -11,11 +11,11 @@ export default function Intro() {
         className="object-cover"
       />
 
-      <div className="absolute inset-0 bg-[#f7f2ec]/50" />
+      <div className="absolute inset-0 bg-[#f7f2ec]/0" />
       <div className="relative grid grid-cols-2 h-full items-center justify-center">
         <div className="mr-9">
             <Image
-                src="/Layer 12.png"
+                src="/profile.png"
                 alt=""
                 width={500}
                 height={500}
@@ -23,22 +23,22 @@ export default function Intro() {
             />
         </div>
         <div className="flex flex-col">
-            <div className="text-black mb-2 text-[16px]">
-                Welcome to <span className="text-[#8f662d] font-semibold">Mayuri Yadav & Associates</span>
+            <div className="text-black mb-2 text-[22px] pb-7">
+                Welcome to <span className="text-[#8f662d] font-bold">Mayuri Yadav & Associates</span>
             </div>
-            <div className="text-[#372d1f]">
+            <div className="text-[#372d1f] text-[30px] font-semibold">
                 Trusted Legal Representation.
             </div>
-            <div className="text-[#372d1f]">
+            <div className="text-[#372d1f] text-[30px] font-semibold">
                 Practical Solutions.
             </div>
-            <div className="text-[#372d1f]">
+            <div className="text-[#372d1f] text-[30px] font-semibold">
                 Client-Centered Approach.
             </div>
-            <div>
+            <div className="mt-6">
                 <button
                 type="submit"
-                className="bg-white rounded-lg bg-linear-to-b from-[#7e6643] to-[#ab9165] px-6 py-3 text-[16px] text-white transition hover:opacity-90 cursor-pointer"
+                className="rounded-lg bg-linear-to-b from-[#7e6643] to-[#ab9165] px-6 py-3 text-[15px] text-white font-semibold cursor-pointer"
                 >
                     EXPLORE OUR SERVICES →
                 </button>
