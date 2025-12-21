23e77d06e55886be7d1fc75070345b8a2f6034e1
Priyanshu Verma <priyanshuvr1308@gmail.com>
2025-12-21 19:38:07 +0530

    homepage-changes

--- src/core/component/intro2.tsx
+++ src/core/component/intro2.tsx
@@ -5,7 +5,6 @@ export default function Intro2() {
     <div className="max-w-6xl mx-auto px-6 my-16 md:my-24">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-20 items-center">
 
-        {/* Text */}
         <div className="space-y-6 max-w-lg">
           <p className="text-sm md:text-base text-gray-700 leading-relaxed">
             <span className="text-[#8f662d] font-semibold">
@@ -25,7 +24,6 @@ export default function Intro2() {
           </p>
         </div>
 
-        {/* Images */}
         <div className="relative w-fit mx-auto md:ml-auto">
           <Image
             src="/profile2.png"
@@ -35,7 +33,7 @@ export default function Intro2() {
             className="rounded-2xl object-cover"
           />
 
-          <div className="absolute -left-16 md:-left-31 bottom-5 md:bottom-7 rounded-xl shadow-xl">
+          <div className="absolute -left-10 md:-left-31 bottom-5 md:bottom-7 rounded-xl shadow-xl">
             <Image
               src="/statue2.png"
               alt="Justice statue"
