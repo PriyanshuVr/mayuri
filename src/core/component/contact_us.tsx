5b890968df0d90d36c78a0260c3ada9174fb6b93
Priyanshu Verma <priyanshuvr1308@gmail.com>
2026-01-22 14:19:47 +0530

    contact_usFinalled

--- src/core/component/contact_us.tsx
+++ src/core/component/contact_us.tsx
@@ -1,6 +1,44 @@
+"use client";
+
 import { Phone, Mail, Clock4, MapPin, House } from "lucide-react";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 export default function ContactUs() {
+
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!formRef.current) return;
+    setLoading(true);
+    emailjs.sendForm(
+      "service_hhweazs",
+      "template_21litpp",
+      formRef.current,
+      "8WWoli8jjGN25e-Y_"
+    )
+      .then(() => {
+        setShowPopup(true);
+        formRef.current?.reset();
+
+        setTimeout(() => {
+          setShowPopup(false);
+        }, 10000);
+
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+        alert("Failed to send message ❌");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="py-12">
       <h1 className="text-4xl md:text-5xl lg:text-7xl font-serif mb-28 text-[#372d1f] text-center">
@@ -56,51 +94,87 @@ export default function ContactUs() {
         </div>
         <div className="flex justify-start lg:justify-end">
           <div className="w-full max-w-md rounded-2xl bg-[#8b6a3f] p-6 shadow-lg">
-            <form className="space-y-4">
+            <form ref={formRef} onSubmit={handleSubmit} className="space-y-4">
+
               <input
                 type="text"
+                name="user_name"
                 placeholder="Your Name"
-                className="bg-white w-full rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none"
+                required
+                className="bg-white w-full rounded-lg px-4 py-3 text-black"
               />
 
               <input
                 type="email"
+                name="user_email"
                 placeholder="Your Email"
-                className="bg-white w-full rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none"
+                required
+                className="bg-white w-full rounded-lg px-4 py-3 text-black"
               />
 
               <input
                 type="tel"
+                name="user_phone"
                 placeholder="Your Phone"
-                inputMode="numeric"
-                pattern="[0-9]*"
-                minLength={10}
-                maxLength={10}
-                className="bg-white w-full rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none"
+                required
+                className="bg-white w-full rounded-lg px-4 py-3 text-black"
               />
 
               <input
                 type="text"
+                name="subject"
                 placeholder="Subject"
-                className="bg-white w-full rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none"
+                required
+                className="bg-white w-full rounded-lg px-4 py-3 text-black"
               />
 
-              <input
-                type="text"
+              <textarea
+                name="message"
                 placeholder="Message"
-                className="bg-white w-full resize-none rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none"
+                required
+                className="bg-white w-full rounded-lg px-4 py-3 h-28 resize-none text-black"
               />
 
               <button
                 type="submit"
-                className="bg-white w-full rounded-lg bg-linear-to-b from-[#dd9229] to-[#d2983c] py-3 text-lg font-semibold text-white transition hover:opacity-90 cursor-pointer"
+                disabled={loading}
+                className="w-full rounded-lg bg-linear-to-b from-[#dd9229] to-[#d2983c] py-3 text-lg font-semibold text-white disabled:opacity-60 disabled:cursor-not-allowed"
+
               >
-                SUBMIT
+                {loading ? "Sending..." : "SUBMIT"}
               </button>
+
             </form>
           </div>
         </div>
       </div>
+      {
+        showPopup && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black/60 z-50">
+            <div className="bg-white rounded-xl p-6 w-[320px] text-center shadow-xl transition-all duration-300 scale-100 animate-in fade-in zoom-in">
+
+
+              <h2 className="text-xl font-semibold text-green-600">
+                ✅ Message Sent
+              </h2>
+
+              <p className="text-gray-600 mt-2">
+                Your message has been delivered successfully.
+              </p>
+
+              <button
+                onClick={() => setShowPopup(false)}
+                className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+              >
+                OK
+              </button>
+
+            </div>
+          </div>
+        )
+      }
+
     </div>
   );
 }
+
