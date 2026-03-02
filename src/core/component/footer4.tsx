7ae61f09f1e6fac29a5dcb34bc77c9d240413945
Priyanshu Verma <priyanshuvr1308@gmail.com>
2026-03-02 13:33:20 +0530

    footer4-changes

--- src/core/component/footer4.tsx
+++ src/core/component/footer4.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer4() {
   return (
-    <div className="relative h-80 w-full overflow-hidden">
+    <div className="relative h-100 w-full overflow-hidden">
       <Image
         src="/library.jpg"
         alt="Library"
@@ -14,8 +14,8 @@ export default function Footer4() {
       <div className="absolute inset-0 bg-[#8b6a3f]/90" />
 
       <div className="relative z-10 flex h-full items-center justify-center px-6 md:px-4 md:mx-40">
-        <div className="max-w-3xl text-center text-white">
-          <h2 className="text-3xl md:text-5xl font-serif mb-6">
+        <div className="text-center text-white">
+          <h2 className="text-3xl md:text-7xl font-serif mb-6">
             Philosophy
           </h2>
 
